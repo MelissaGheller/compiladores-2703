5fd64b8742f3e74beb74fc768c83e3b487975525
Melissa CG <melissaghellerc@gmail.com>
2024-03-24 19:44:07 -0300

    teste

--- src/zero.py
+++ src/zero.py
@@ -7,9 +7,23 @@ def procura_maior(lista):
             maior = item
     return maior
 
-def procura_menor(dados: list) -> int:
-    menor = dados[0]
-    for item in dados[1:]:
+def procura_menor(lista):
+    menor = lista[0]
+    for item in lista[1:]:
         if item < menor:
             menor = item
     return menor
+
+def procura_pares(lista):
+    pares = []
+    for item in lista:
+        if item % 2 == 0:
+            pares.append(item)
+    return pares
+
+def procura_impares(lista):
+    impares = []
+    for item in lista:
+        if item % 2 != 0:
+            impares.append(item)
+    return impares
